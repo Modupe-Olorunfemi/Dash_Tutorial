6b234b7b9758428b398325e9998e75014efe49db
Modupe Olorunfemi <Modupe.Olorunfemi@dhsc.gov.uk>
2024-08-21 16:01:57 +0100

    concluded hello world

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
-from dash import Dash, html
+# Import packages
+from dash import Dash, html, dash_table
+import pandas as pd
+
+# Initialize the app
 app = Dash()
+
+# App layout
 app.layout = [html.Div(children='Hello World')]
+
+# Run the app
+
 if __name__ == '__main__':
+
     app.run(debug=True)
+
+
+
+
+
+
+
+
+ 
+
+
