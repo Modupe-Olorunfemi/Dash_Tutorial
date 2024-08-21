647113c4abb63128f348a75d00cd952666699a6c
Modupe Olorunfemi <Modupe.Olorunfemi@dhsc.gov.uk>
2024-08-21 16:47:51 +0100

    visualising data

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 # Import packages
-from dash import Dash, html, dash_table
+from dash import Dash, html, dash_table, dcc
 import pandas as pd
+import plotly.express as px
 
 # Initialize the app
 app = Dash()
 
-# Incorporate data
+# Incorporating data
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
 # App layout
 app.layout = [
-    html.Div(children='My First App with Data'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+    html.Div(children='My First App with Data and a Graph'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+    dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
 ]
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
