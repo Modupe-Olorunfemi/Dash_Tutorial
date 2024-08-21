02e82fc3f2f9f2c59a7f0d6661376f2a22fe749a
Modupe Olorunfemi <Modupe.Olorunfemi@dhsc.gov.uk>
2024-08-21 16:34:19 +0100

    connecting to Data

--- app.py
+++ app.py
@@ -5,14 +5,18 @@ import pandas as pd
 # Initialize the app
 app = Dash()
 
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
 # App layout
-app.layout = [html.Div(children='Hello World')]
-
+app.layout = [
+    html.Div(children='My First App with Data'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+]
 # Run the app
-
 if __name__ == '__main__':
-
     app.run(debug=True)
+
 
 
 
