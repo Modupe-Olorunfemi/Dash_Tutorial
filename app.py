c807b603d4d23be08790e877de2f8cd0cab2cb92
Modupe Olorunfemi <Modupe.Olorunfemi@dhsc.gov.uk>
2024-08-21 20:18:21 +0100

    Dash Design Kit

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-# STYLING APP HTML and CSS
+# STYLING APP Dash Design Kit(DDK)
 # Import packages
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
+import dash_design_kit as ddk
 
 
 
@@ -10,41 +11,34 @@ import plotly.express as px
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
 #Initializing the app - incorporating CSS
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = Dash(external_stylesheets=external_stylesheets)
+app = Dash(__name__)
 
 
 # App layout
-app.layout = [
-    html.Div(className='row', children='My First App with Data, Graph, and Controls',
-             style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
-
-    html.Div(className='row', children=[
-        dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'],
-                       value='lifeExp',
-                       inline=True,
-                       id='my-radio-buttons-final')
+app.layout = ddk.App([
+    ddk.Header(ddk.Title('My First App with Data, Graph, and Controls')),
+    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'],
+                    value='lifeExp',
+                    inline=True,
+                    id='my-ddk-radio-items-final'),
+    ddk.Row([
+        ddk.Card([
+            dash_table.DataTable(data=df.to_dict('records'), page_size=12, style_table={'overflowX': 'auto'})
+        ], width=50),
+        ddk.Card([
+            ddk.Graph(figure={}, id='graph-placeholder-ddk-final')
+        ], width=50),
     ]),
 
-    html.Div(className='row', children=[
-        html.Div(className='six columns', children=[
-            dash_table.DataTable(data=df.to_dict('records'), page_size=11, style_table={'overflowX': 'auto'})
-        ]),
-        html.Div(className='six columns', children=[
-            dcc.Graph(figure={}, id='histo-chart-final')
-        ])
-    ])
-]
-
+])
 # Add controls to build the interaction
 @callback(
-    Output(component_id='histo-chart-final', component_property='figure'),
-    Input(component_id='my-radio-buttons-final', component_property='value')
+    Output(component_id='graph-placeholder-ddk-final', component_property='figure'),
+    Input(component_id='my-ddk-radio-items-final', component_property='value')
 )
 def update_graph(col_chosen):
     fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
     return fig
-
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
